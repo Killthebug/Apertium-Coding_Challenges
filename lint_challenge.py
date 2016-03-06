ed448a7cf67f10771e1fd77bb3c0aa69c129e9c3
jpsinghgoud <jaipal.singh@research.iiit.ac.in>
2016-03-06 14:49:19 +0530

    BKUP

---
+++ lint_challenge.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import re
+import sys
+from itertools import groupby
+
+def extractBetween(line, start, end):
+    """
+    Extract substring between two strings
+    """
+    try:
+        head = line.index(start) + len(start)
+        tail = line.index(end, head)
+        return line[head:tail]
+    except ValueError:
+        return ""
+
+def printList(List):
+    for iterator in List:
+        print iterator
+
+def main():
+    lines = []
+    result = []
+    dix = sys.argv[1]
+    pattern = re.compile(r'<e lm=.*</e>')
+   
+    #Filter the lines that we need to extract info from
+    with open(dix) as myFile:
+        for match, y in groupby(myFile, key=lambda m:pattern.search(m)):
+            if match:
+                lines.append(''.join(list(y)))
+   
+    #Parse each line and extract information
+    for line in lines:
+        lemma = extractBetween(line, 'lm="', '">')
+        surface = extractBetween(line, '<i>', '</i>')
+        if surface == '':
+            surface = extractBetween(line, '<b/>', '</l>')
+        paradigm = extractBetween(line, '<par n="', '"/>')
+        print result
+        temp = (lemma, surface, paradigm)
+        result.append(temp)
+    
+    printList(result)
+    exit(1)
+
+if __name__ == '__main__':
+    main()
